b2e4155e8c46b61ae5579809a34a842ab431f9f7
Corey Roth <info@coreyaroth.com>
2019-03-27 13:44:32 -0400

    fixed card generator

--- cards-list.php
+++ cards-list.php
@@ -44,30 +44,45 @@ Template Name: Card Deck
 				<button class="show centered">Show All</button>
 			</div>
 			<div class="flex-row card-flex">
-					<?php
-					//$activecategory = get_field('recipe_number');
-				    $args = array( 'category' => 1358 , 'post_type' =>  'post' ); 
-				    $postslist = get_posts( $args );    
-				    foreach ($postslist as $post) :  setup_postdata($post); 
-				    ?>
-				    <div class="styled-card mini" data-category="<?php $category = get_the_category(); $firstCategory = $category[1]->cat_name; echo $firstCategory;?>" 
-				    	data-recipe="
-				    	<?php
-				    	$args = array('child_of' => 1359);
-						$post_categories = wp_get_post_categories( get_the_ID() );
-						$cats = array();
-						foreach($post_categories as $c){
-						    $cat = get_category( $c );
-						    echo $cat->slug . ' ';
-						    $cats[] = array( 'name' => $cat->name, 'slug' => $cat->slug );
+				<?php 
+				// the query
+				$wpb_all_query = new WP_Query(array('post_type'=>'post', 'post_status'=>'publish', 'posts_per_page'=>-1)); ?>
+				<?php if ( $wpb_all_query->have_posts() ) : ?>
+				<?php while ( $wpb_all_query->have_posts() ) : $wpb_all_query->the_post(); ?>
+				<div class="styled-card mini" data-category="<?php 
+			    	$args = array('child_of' => 1358);
+					$post_categories = wp_get_post_categories( get_the_ID($args) );
+					foreach($post_categories as $c){
+					    $cat = get_category( $c );
+					    if ($cat->name == 'Look In' || $cat->name == 'Look Out' || $cat->name == 'Look Forward' || $cat->name == 'Expand' || $cat->name == 'Focus') {
+					    	$cat_name = ucfirst($cat->name);
+						    echo $cat_name;
+					    } else {
+					    	//do nothing
 						}
-						?>"> 
+					}?>" 
+			    	data-recipe="
+			    	<?php
+			    	$args = array('child_of' => 1359);
+					$post_categories = wp_get_post_categories( get_the_ID() );
+					foreach($post_categories as $c){
+					    $cat = get_category( $c );
+					    echo $cat->slug . ' ';
+					}
+					?>"> 
 
-					    <h3><?php the_title(); ?></h3> 
-					    <?php the_field('short_description'); ?>
-					    <a href="<?php the_permalink(); ?>">View Card</a>
-					</div>
-				    <?php endforeach; ?>
+				    <h3><?php the_title(); ?></h3> 
+				    <?php the_field('short_description'); ?>
+				    <a href="<?php the_permalink(); ?>">View Card</a>
+				</div>
+				<?php endwhile; ?>
+				<!-- end of the loop -->
+
+				<?php wp_reset_postdata(); ?>
+
+				<?php else : ?>
+				<p><?php _e( 'Sorry, no posts matched your criteria.' ); ?></p>
+				<?php endif; ?>
 			</div>
 		</section>
 	<!-- /section -->
